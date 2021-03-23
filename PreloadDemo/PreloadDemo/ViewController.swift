46bb23e311f4a04939300d2554a6ba3c40daa5df
ShenJieSuzhou <745689122@qq.com>
2021-03-23 14:10:21 +0800

    网络预加载工程初始化
    网络预加载工程初始化

---
+++ PreloadDemo/PreloadDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PreloadDemo
+//
+//  Created by shenjie on 2021/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
