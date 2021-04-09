28db24b2d8bd41f32a39897429416d430251392d
ShenJieSuzhou <745689122@qq.com>
2021-04-09 14:12:28 +0800

    提交 NUKE Demo 以供参考

---
+++ 参考/NukeDemo-main/Sources/PrefetchSwiftUIDemo.swift
@@ -0,0 +1,91 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2015-2021 Alexander Grebenyuk (github.com/kean).
+
+import Foundation
+import SwiftUI
+import FetchImage
+import ScrollViewPrefetcher
+import Nuke
+
+// MARK: - View
+
+@available(iOS 14.0, *)
+struct PrefetchDemoView: View {
+    @StateObject var model = PrefetchDemoViewModel()
+
+    var body: some View {
+        GeometryReader { geometry in
+            ScrollView {
+                let side = geometry.size.width / 4
+                let item = GridItem(.fixed(side), spacing: 2)
+                LazyVGrid(columns: Array(repeating: item, count: 4), spacing: 2) {
+                    ForEach(demoPhotosURLs.indices) { index in
+                        ImageView(url: demoPhotosURLs[index])
+                            .frame(width: side, height: side)
+                            .clipped()
+                            .onAppear { model.onAppear(index) }
+                            .onDisappear { model.onDisappear(index) }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@available(iOS 14.0, *)
+struct ImageView: View {
+    let url: URL
+
+    @StateObject private var image = FetchImage()
+
+    var body: some View {
+        ZStack {
+            Rectangle().fill(Color.gray)
+            image.view?
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .clipped()
+        }
+        .onAppear { image.load(url) }
+        .onDisappear(perform: image.reset)
+    }
+}
+
+// MARK: - ViewModel
+
+final class PrefetchDemoViewModel: ObservableObject, ScrollViewPrefetcherDelegate {
+    private let imagePrefetcher: ImagePrefetcher
+    private let scrollViewPrefetcer: ScrollViewPrefetcher
+    let urls: [URL]
+
+    init() {
+        self.imagePrefetcher = ImagePrefetcher()
+        self.scrollViewPrefetcer = ScrollViewPrefetcher()
+        self.urls = demoPhotosURLs
+
+        self.scrollViewPrefetcer.delegate = self
+    }
+
+    func onAppear(_ index: Int) {
+        scrollViewPrefetcer.onAppear(index)
+    }
+
+    func onDisappear(_ index: Int) {
+        scrollViewPrefetcer.onDisappear(index)
+    }
+
+    // MARK: ScrollViewPrefetcherDelegate
+
+    func getAllIndicesForPefetcher(_ prefetcher: ScrollViewPrefetcher) -> Range<Int> {
+        urls.indices
+    }
+
+    func prefetcher(_ prefetcher: ScrollViewPrefetcher, prefetchItemsAt indices: [Int]) {
+        imagePrefetcher.startPrefetching(with: indices.map { urls[$0] })
+    }
+
+    func prefetcher(_ prefetcher: ScrollViewPrefetcher, cancelPrefechingForItemAt indices: [Int]) {
+        imagePrefetcher.stopPrefetching(with: indices.map { urls[$0] })
+    }
+}
---
+++ 参考/NukeDemo-main/Sources/PrefetchingDemoViewController.swift
@@ -0,0 +1,60 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2015-2021 Alexander Grebenyuk (github.com/kean).
+
+import UIKit
+import Nuke
+
+private let cellReuseID = "reuseID"
+private var loggingEnabled = true
+
+final class PrefetchingDemoViewController: BasicDemoViewController, UICollectionViewDataSourcePrefetching {
+    let prefetcher = ImagePrefetcher()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        collectionView?.isPrefetchingEnabled = true
+        collectionView?.prefetchDataSource = self
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        prefetcher.isPaused = false
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+
+        prefetcher.isPaused = true
+    }
+
+    // MARK: UICollectionViewDataSourcePrefetching
+
+    func collectionView(_ collectionView: UICollectionView, prefetchItemsAt indexPaths: [IndexPath]) {
+        let urls = indexPaths.map { photos[$0.row] }
+        prefetcher.startPrefetching(with: urls)
+        if loggingEnabled {
+            print("prefetchItemsAt: \(stringForIndexPaths(indexPaths))")
+        }
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cancelPrefetchingForItemsAt indexPaths: [IndexPath]) {
+        let urls = indexPaths.map { photos[$0.row] }
+        prefetcher.startPrefetching(with: urls)
+        if loggingEnabled {
+            print("cancelPrefetchingForItemsAt: \(stringForIndexPaths(indexPaths))")
+        }
+    }
+}
+
+private func stringForIndexPaths(_ indexPaths: [IndexPath]) -> String {
+    guard indexPaths.count > 0 else {
+        return "[]"
+    }
+    let items = indexPaths
+        .map { return "\(($0 as NSIndexPath).item)" }
+        .joined(separator: " ")
+    return "[\(items)]"
+}
