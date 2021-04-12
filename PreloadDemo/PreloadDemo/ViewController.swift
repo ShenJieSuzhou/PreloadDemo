7995d3f54d64662e116aaf176f09dc3fc4c4dcec
shenjie <745689122@qq.com>
2021-04-12 23:48:34 +0800

    测试预加载哪种方式更优

--- PreloadDemo/PreloadDemo/ViewController.swift
+++ PreloadDemo/PreloadDemo/ViewController.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-import Kingfisher
+//import Kingfisher
 
 class ViewController: UIViewController {
     
@@ -57,55 +57,16 @@ extension ViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         // preheat image ，处理将要显示的图像
-//        guard let cell = cell as? ProloadTableViewCell else {
-//            return
-//        }
-//
-//        // 图片下载完毕后更新 cell
-//        let updateCellClosure: (UIImage?) -> () = { [unowned self] (image) in
-//            cell.updateUI(image, orderNo: "\(indexPath.row)")
-//            viewModel.loadingOperations.removeValue(forKey: indexPath)
-//        }
-//
-//        // 1. 首先判断是否已经存在创建好的下载线程
-//        if let dataLoader = viewModel.loadingOperations[indexPath] {
-//            if let image = dataLoader.image {
-//                // 1.1 若图片已经下载好，直接更新
-//                cell.updateUI(image, orderNo: "\(indexPath.row)")
-//            } else {
-//                // 1.2 若图片还未下载好，则等待图片下载完后更新 cell
-//                dataLoader.loadingCompleteHandle = updateCellClosure
-//            }
-//        } else {
-//            // 2. 没找到，则为指定的 url 创建一个新的下载线程
-//            print("在 \(indexPath.row) 行创建一个新的图片下载线程")
-//            if let dataloader = viewModel.loadImage(at: indexPath.row) {
-//                // 2.1 添加图片下载完毕后的回调
-//                dataloader.loadingCompleteHandle = updateCellClosure
-//                // 2.2 启动下载
-//                viewModel.loadingQueue.addOperation(dataloader)
-//                // 2.3 将该下载线程加入到记录数组中以便根据索引查找
-//                viewModel.loadingOperations[indexPath] = dataloader
-//            }
-//        }
-    }
-}
-
-extension ViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return viewModel.totalCount
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "PreloadCellID") as? ProloadTableViewCell else {
-            fatalError("Sorry, could not load cell")
+        guard let cell = cell as? ProloadTableViewCell else {
+            return
         }
-        
+
+        // 图片下载完毕后更新 cell
         let updateCellClosure: (UIImage?) -> () = { [unowned self] (image) in
             cell.updateUI(image, orderNo: "\(indexPath.row)")
             viewModel.loadingOperations.removeValue(forKey: indexPath)
         }
-        
+
         // 1. 首先判断是否已经存在创建好的下载线程
         if let dataLoader = viewModel.loadingOperations[indexPath] {
             if let image = dataLoader.image {
@@ -127,9 +88,34 @@ extension ViewController: UITableViewDataSource {
                 viewModel.loadingOperations[indexPath] = dataloader
             }
         }
+    }
+    
+    func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        // If there's a data loader for this index path we don't need it any more. Cancel and dispose
+//        if let dataLoader = loadingOperations[indexPath] {
+//            dataLoader.cancel()
+//            loadingOperations.removeValue(forKey: indexPath)
+//        }
         
+        if let dataLoader = viewModel.loadingOperations[indexPath] {
+            print("在 \(indexPath.row) 行取消下载线程")
+            dataLoader.cancel()
+            viewModel.loadingOperations.removeValue(forKey: indexPath)
+        }
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.totalCount
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "PreloadCellID") as? ProloadTableViewCell else {
+            fatalError("Sorry, could not load cell")
+        }
         
-//        cell.updateUI(.none, orderNo: "\(indexPath.row)")
+        cell.updateUI(.none, orderNo: "\(indexPath.row)")
         return cell
     }
 }
